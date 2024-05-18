6bf136874d25871eb732aacbee96913bf092be85
Mehdi Samsami <mehdisamsami@live.com>
2024-05-18 08:55:04 +0330

    change import

--- src/me_llm/__init__.py
+++ src/me_llm/__init__.py
@@ -3,6 +3,6 @@ __version__ = "0.0.1"
 
 from ._typing import UserProfile
 from .embed import LiteLLMEmbeddings
-from .main import MeLLM
+from .core import MeLLM
 
 __all__ = ("LiteLLMEmbeddings", "MeLLM", "UserProfile")
