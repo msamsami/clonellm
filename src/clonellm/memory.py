307cffbbc774431412947f6759a5b7d5466752bc
Mehdi Samsami <mehdisamsami@live.com>
2024-06-21 13:46:48 +0330

    use max_memory_size for memory class, use an internal method for trimming the message list

--- src/clonellm/memory.py
+++ src/clonellm/memory.py
@@ -1,10 +1,8 @@
-from typing import Sequence
+from typing import cast, Sequence
 
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import BaseMessage
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-_store = {}
+from langchain_core.pydantic_v1 import BaseModel, Field, root_validator
 
 __all__ = ("InMemoryHistory", "get_session_history", "get_session_history_size", "clear_session_history")
 
@@ -13,7 +11,18 @@ class InMemoryHistory(BaseChatMessageHistory, BaseModel):
     """In memory implementation of chat message history."""
 
     messages: list[BaseMessage] = Field(default_factory=list)
-    memory: int = -1
+    max_memory_size: int = Field(
+        default=-1, description="Maximum number of messages in memory. -1 means no limit and 0 means no memory at all.", ge=-1
+    )
+
+    @staticmethod
+    def _trim_messages(messages: Sequence[BaseMessage], max_memory_size: int) -> Sequence[BaseMessage]:
+        if max_memory_size == 0:
+            return []
+        elif max_memory_size > 0:
+            return messages[-max_memory_size:]
+        else:
+            return messages
 
     def add_messages(self, messages: Sequence[BaseMessage]) -> None:
         """Add a list of messages to the store
@@ -24,16 +33,27 @@ class InMemoryHistory(BaseChatMessageHistory, BaseModel):
         """
         for message in messages:
             self.messages.append(message)
-        if self.memory > 0:
-            self.messages = self.messages[-self.memory :]
+        self.messages = cast(list[BaseMessage], self._trim_messages(self.messages, self.max_memory_size))
+
+    @root_validator
+    def trim_messages_upon_init(cls, values):
+        values["messages"] = cls._trim_messages(values["messages"], values["max_memory_size"])
+        return values
+
+    @property
+    def memory_size(self) -> int:
+        return len(self.messages)
 
     def clear(self) -> None:
         self.messages = []
 
 
-def get_session_history(session_id: str) -> InMemoryHistory:
+_store: dict[str, InMemoryHistory] = {}
+
+
+def get_session_history(session_id: str, max_memory_size: int = -1) -> InMemoryHistory:
     if session_id not in _store:
-        _store[session_id] = InMemoryHistory()
+        _store[session_id] = InMemoryHistory(max_memory_size=max_memory_size)
     return _store[session_id]
 
 
