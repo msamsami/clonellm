fc66dbb1dc12e5b227895cdd4b5931a0230b1188
Mehdi Samsami <mehdisamsami@live.com>
2024-09-26 12:47:35 +0330

    remove todo section from readme, bump version to 0.2.2

--- src/clonellm/__init__.py
+++ src/clonellm/__init__.py
@@ -1,9 +1,9 @@
 __author__ = "Mehdi Samsami"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
-from ._typing import UserProfile
 from .core import CloneLLM
 from .embed import LiteLLMEmbeddings
 from .enums import RagVectorStore
+from .models import UserProfile
 
 __all__ = ("LiteLLMEmbeddings", "CloneLLM", "RagVectorStore", "UserProfile")
